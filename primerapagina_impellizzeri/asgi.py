66b5fccbe50afa2e20ba03b7ddbde41f15c7a2b4
Martin Impellizzeri <impellizzerimartin@gmail.com>
2023-09-30 00:03:30 -0700

    Starting my page project

---
+++ primerapagina_impellizzeri/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for primerapagina_impellizzeri project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'primerapagina_impellizzeri.settings')
+
+application = get_asgi_application()
